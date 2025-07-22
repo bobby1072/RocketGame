1dc46f75d89af5dd5b83c3c2ddab5d3ed2beb25b
bobby1072 <bobby.thompson1072@gmail.com>
2025-07-23 00:59:46 +0100

    OLne test

---
+++ src/PokeGame.Core/PokeGame.Core.Domain.Services.Tests/PokdexTests/CreatePokedexPokemonCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture;
+using BT.Common.Persistence.Shared.Models;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using PokeGame.Core.Domain.Services.Pokedex.Commands;
+using PokeGame.Core.Persistence.Repositories.Abstract;
+using PokeGame.Core.Schemas;
+
+namespace PokeGame.Core.Domain.Services.Tests.PokdexTests;
+
+public sealed class CreatePokedexPokemonCommandTests
+{
+    private readonly Fixture _fixture = new();
+    private readonly Mock<IPokedexPokemonRepository> _mockPokedexPokemonRepository = new();
+    private readonly CreatePokedexPokemonCommand _command;
+
+    public CreatePokedexPokemonCommandTests()
+    {
+        _command = new CreatePokedexPokemonCommand(
+            _mockPokedexPokemonRepository.Object,
+            new NullLogger<CreatePokedexPokemonCommand>()
+        );
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_Should_Create_Only_New_Pokemon()
+    {
+        //Arrange
+        var originalPokemon = 
+            _fixture
+                .CreateMany<PokedexPokemon>().ToArray();
+        
+        _mockPokedexPokemonRepository
+            .Setup(x => x.GetAll())
+            .ReturnsAsync(new DbGetManyResult<PokedexPokemon>(originalPokemon));
+        
+        //Act
+        var result = await _command.ExecuteAsync(originalPokemon);
+        
+        //Assert
+        Assert.Equal([], result);
+        
+        _mockPokedexPokemonRepository.Verify(x => x.GetAll(), Times.Once);
+        _mockPokedexPokemonRepository.VerifyNoOtherCalls();
+    }
+}
