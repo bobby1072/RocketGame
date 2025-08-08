58475c927b8b3a78ef13d71d70042d1221b963e7
Bobby Thompson <bobbythompson@paypoint.com>
2025-08-08 12:53:11 +0100

    Added context to main

--- src/phaser-client/src/main.tsx
+++ src/phaser-client/src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AppSettingsContextProvider } from "./common/contexts/AppSettingsContext.tsx";
 import { PokeGameCoreHttpClientContextProvider } from "./common/contexts/PokeGameCoreHttpClientContext.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <AppSettingsContextProvider>
             <PokeGameCoreHttpClientContextProvider>
-                <App />
+                <QueryClientProvider client={new QueryClient()}>
+                    <App />
+                </QueryClientProvider>
             </PokeGameCoreHttpClientContextProvider>
         </AppSettingsContextProvider>
     </React.StrictMode>
