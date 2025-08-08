4884b869f7a86d597ebf93dcdeed0a970d310383
Bobby Thompson <bobbythompson@paypoint.com>
2025-08-08 11:22:24 +0100

    Wrapped app in conversation

--- src/phaser-client/src/main.tsx
+++ src/phaser-client/src/main.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { AppSettingsContextProvider } from "./common/contexts/AppSettingsContext.tsx";
+import { PokeGameCoreHttpClientContextProvider } from "./common/contexts/PokeGameCoreHttpClientContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-)
+        <AppSettingsContextProvider>
+            <PokeGameCoreHttpClientContextProvider>
+                <App />
+            </PokeGameCoreHttpClientContextProvider>
+        </AppSettingsContextProvider>
+    </React.StrictMode>
+);
